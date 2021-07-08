2ac0416845bc0e05cf220d45460d9fbf8e2f976c
hiraitomoyuki <kotemo0605@gmail.com>
2021-07-08 10:34:34 +0000

    Initial Commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,55 @@
+class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    if @book.save
+      redirect_to book_path, notice: 'Book was successfully updated.'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
+  end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
+
+    def book_params
+      params.require(:book).permit(:title, :body)
+    end
+end
